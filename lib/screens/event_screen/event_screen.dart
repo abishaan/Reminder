c09c6d4320958537474cc28b3d696de074105526
Praveenkumar Srikanthan <it17137324@my.sliit.lk>
2020-03-25 17:10:58 +0530

    working v1.1:- refactored

--- lib/screens/event_screen/event_screen.dart
+++ lib/screens/event_screen/event_screen.dart
@@ -1,9 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:reminder/models/event.dart';
-import 'package:reminder/screens/event_screen/event_list.dart';
+import 'package:reminder/screens/event_screen/event_card.dart';
+import 'package:reminder/screens/event_screen/profile_label.dart';
 import 'package:reminder/services/event_service.dart';
+import 'package:reminder/shared/empty_image_widget.dart';
 
 class EventScreen extends StatelessWidget {
   @override
@@ -11,7 +12,42 @@ class EventScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: EventList(),
+        body: StreamBuilder<QuerySnapshot>(
+            stream: EventService().getCurrentEventSnapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return LinearProgressIndicator();
+              return snapshot.data.documents.length > 0
+                  ? ListView(
+                      children: <Widget>[
+                        ProfileLabel(),
+                        Column(
+                          children: snapshot.data.documents
+                              .asMap()
+                              .map(
+                                (index, data) => MapEntry(
+                                  index,
+                                  EventCard(
+                                    index: index,
+                                    remindEvent: RemindEvent.fromSnapshot(data),
+                                  ),
+                                ),
+                              )
+                              .values
+                              .toList(),
+                        ),
+                      ],
+                    )
+                  : ListView(
+                      children: <Widget>[
+                        ProfileLabel(),
+                        EmptyImageWidget(
+                            title: 'You have a free day.',
+                            subtitle:
+                                'Ready for some new events? Tap + to write them down.',
+                            imagePath: 'assets/images/archive.png'),
+                      ],
+                    );
+            }),
       ),
     );
   }
